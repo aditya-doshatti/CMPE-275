b534380ac317472d6f378d7b07cdd7f7fab14811
Aditya Doshatti <aditya.doshatti@sjsu.edu>
2019-03-03 15:22:18 -0800

    First commit

---
+++ cmpe275-lab-aop-sp19-v1/cmpe275-lab-aop-secret/src/main/java/edu/sjsu/cmpe275/aop/SecretStatsImpl.java
@@ -0,0 +1,37 @@
+package edu.sjsu.cmpe275.aop;
+
+public class SecretStatsImpl implements SecretStats {
+    /***
+     * Following is a dummy implementation.
+     * You are expected to provide an actual implementation based on the requirements.
+     */
+
+	public void resetStatsAndSystem() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int getLengthOfLongestSecret() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String getMostTrustedUser() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getWorstSecretKeeper() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getBestKnownSecret() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+    
+}
+
+
+
